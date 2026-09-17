364b43d3ab4ce1790eb2cab7b7b1a40554d2ccfe
unknown <1315734171@qq.com>
2026-09-17 16:42:30 +0800

    d01: python 环境 + 函数练习

---
+++ learning‑log/python/day01.py
@@ -0,0 +1,14 @@
+print("hello world")
+def add(a,b):
+    return a+b
+print(add(1,2))
+def add(current,previous):
+    return current + previous
+print(add(1,2))
+def is_enough(point, need):
+    # 如果 point >= need 返回 True，否则返回 False
+    if point >= need:
+        return  True
+    else:
+        return False
+print(is_enough(2,3))
