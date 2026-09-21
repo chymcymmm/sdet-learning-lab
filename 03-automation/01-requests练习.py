f700f7270bebfa951d0d68547fdc0024f57df153
unknown <1315734171@qq.com>
2026-09-21 17:21:02 +0800

    03: requests 练习 + 整理文件命名

--- 03-automation/01-requests练习.py
+++ 03-automation/01-requests练习.py
@@ -1,37 +1,42 @@
-from urllib import response
+from sys import platform
 
 import requests
 
 # 第1题｜GET + Query
-# 请求 https://httpbin.org/get，带 Query 参数 name=test，打印 status_code
-response=requests.get('http://httpbin.org/json',
-                      params="name:test")
+# 请求 https://httpbin.org/get，带 Query 参数 name=test
+# 打印 status_code
+response=requests.get('http://httpbin.org/get',
+                      params={"name":"test"})
 print(response.status_code)
+
+
 # 第2题｜取响应字段
 # 请求 https://httpbin.org/get，带 Query 参数 city=beijing
+# 打印 response.json()["args"]
 response=requests.get('http://httpbin.org/get',
-                      json={"city":"beijing"})
-# 打印 response.json()["args"]，看 city 在不在里面
+                      params={"city":"beijing"})
 print(response.json()["args"])
+
+
 # 第3题｜POST + Body
 # 请求 https://httpbin.org/post，带 Body {"userId": 1, "point": 100}
+# 打印 response.json()["json"]
 response=requests.post('http://httpbin.org/post',
                        json={"userId": 1, "point": 100})
-# 打印 response.json()["json"]
 print(response.json()["json"])
 
 
 # 第4题｜Header
 # 请求 https://httpbin.org/get，带 Header token=abc123
-# 打印 response.json()["headers"]，看有没有 token
+# 打印 response.json()["headers"]，确认里面有 token
 response=requests.get('http://httpbin.org/get',
-                      json={"token":"abc123"})
+                      headers={"token": "abc123"})
 print(response.json()["headers"])
 
 # 第5题｜综合
 # 请求 https://httpbin.org/get，带 Query 参数 platform=wechat
 # 1. 打印 status_code
-# 2. 从响应里取出 platform 的值并打印（提示：response.json()["args"]["platform"]）
+# 2. 打印 response.json()["args"]["platform"]
 response=requests.get('http://httpbin.org/get',
-                      params="platform:wechat")
+                      params={"platform": "wechat"})
 print(response.json()["args"]["platform"])
